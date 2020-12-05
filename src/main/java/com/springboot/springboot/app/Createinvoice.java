af731fa1b8f84b5aca5e496ad7867b62b8f6877b
RITESHKUMAR10 <ritesh1021997@gmail.com>
2020-12-05 15:19:03 +0530

    survosys solutions task completed

--- src/main/java/com/springboot/springboot/app/Createinvoice.java
+++ src/main/java/com/springboot/springboot/app/Createinvoice.java
@@ -20,43 +20,59 @@ public class CreateInvoice {
 
 	public static void main(String[] args) {
 		try {
-			//Create workbook in .xlsx format
+			//Creating the  workbook in .xlsx format
 			Workbook workbook = new XSSFWorkbook();
 			//For .xsl workbooks use new HSSFWorkbook();
-			//Create Sheet
+			//Now i am creating Sheet
 			Sheet sh = workbook.createSheet("Invoices");
-			//Create top row with column headings
+			
+			//Creating top row with column headings
+			
 			String[] columnHeadings = {"Item id","Item Name","Qty","Item Price","Sold Date"};
-			//We want to make it bold with a foreground color.
+			//making it bold with a foreground color.
+			
 			Font headerFont = workbook.createFont();
 			headerFont.setBold(true);
 			headerFont.setFontHeightInPoints((short)12);
 			headerFont.setColor(IndexedColors.BLACK.index);
-			//Create a CellStyle with the font
+			
+			//Creating a CellStyle with the font
+			
 			CellStyle headerStyle = workbook.createCellStyle();
 			headerStyle.setFont(headerFont);
 			headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 			headerStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.index);
-			//Create the header row
+			
+			//Creating the header row
+			
 			Row headerRow = sh.createRow(0);
+			
 			//Iterate over the column headings to create columns
+			
 			for(int i=0;i<columnHeadings.length;i++) {
 				Cell cell = headerRow.createCell(i);
 				cell.setCellValue(columnHeadings[i]);
 				cell.setCellStyle(headerStyle);
 			}
-			//Freeze Header Row
+			//Freezing Header Row
+			
 			sh.createFreezePane(0, 1);
-			//Fill data
+			
+			//Filling data logic will be implementing
+			
 			ArrayList<Invoices> a = createData();
 			CreationHelper creationHelper= workbook.getCreationHelper();
 			CellStyle dateStyle = workbook.createCellStyle();
 			dateStyle.setDataFormat(creationHelper.createDataFormat().getFormat("MM/dd/yyyy"));
 			int rownum =1;
 			for(Invoices i : a) {
+				
 				//System.out.println("rownum-before"+(rownum));
+				
 				Row row = sh.createRow(rownum++);
+				
 				//System.out.println("rownum-after"+(rownum));
+				
 				row.createCell(0).setCellValue(i.getItemId());
 				row.createCell(1).setCellValue(i.getItemName());
 				row.createCell(2).setCellValue(i.getItemQty());
@@ -65,11 +81,14 @@ public class CreateInvoice {
 				dateCell.setCellValue(i.getItemSoldDate());
 				dateCell.setCellStyle(dateStyle);
 			}
-			//Group and collapse rows
+			//Group and collapse rows(that is the for collapsing the table result of column and rows).
+			
 			int noOfRows = sh.getLastRowNum();
 			sh.groupRow(1, noOfRows);
 			sh.setRowGroupCollapsed(1, true);
-			//Create a sum row
+			
+			//Creating a sum row
+			
 			Row sumRow = sh.createRow(rownum);
 			Cell sumRowTitle = sumRow.createCell(0);
 			sumRowTitle.setCellValue("Total");
@@ -80,12 +99,15 @@ public class CreateInvoice {
 			sumcell.setCellFormula(strFormula);
 			sumcell.setCellValue(true);
 			
-			//Autosize columns
+			//Autosizing the  columns
+			
 			for(int i=0;i<columnHeadings.length;i++) {
 				sh.autoSizeColumn(i);
 			}
 			Sheet sh2 = workbook.createSheet("Second");
-			//Write the output to file
+			
+			//Writing  the output to file
+			
 			FileOutputStream fileOut = new FileOutputStream("/home/parallels/Documents/docs/invoices.xlsx");
 			workbook.write(fileOut);
 			fileOut.close();
@@ -102,15 +124,15 @@ public class CreateInvoice {
 	a.add(new Invoices(1, "Apple", 2, 15.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/01/2020")));
 	a.add(new Invoices(2, "Orange", 1, 55.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/02/2020")));
 	a.add(new Invoices(3, "Lemon", 5, 105.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/01/2020")));
-	a.add(new Invoices(4, "Pen", 100, 25.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/02/2020")));
+	a.add(new Invoices(4, "PineApple", 100, 25.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/02/2020")));
 	a.add(new Invoices(5, "Apple", 2, 15.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/01/2020")));
 	a.add(new Invoices(6, "Orange", 1, 55.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/02/2020")));
 	a.add(new Invoices(7, "Lemon", 5, 105.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/01/2020")));
-	a.add(new Invoices(8, "Pen", 100, 25.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/02/2020")));
+	a.add(new Invoices(8, "PineApple", 100, 25.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/02/2020")));
 	a.add(new Invoices(9, "Apple", 2, 15.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/01/2020")));
 	a.add(new Invoices(10, "Orange", 1, 55.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/02/2020")));
 	a.add(new Invoices(11, "Lemon", 5, 105.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/01/2020")));
-	a.add(new Invoices(12, "Pen", 100, 25.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/02/2020")));
+	a.add(new Invoices(12, "PineApple", 100, 25.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/02/2020")));
 	a.add(new Invoices(13, "Apple", 2, 15.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/01/2020")));
 	a.add(new Invoices(14, "Orange", 1, 55.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/02/2020")));
 	a.add(new Invoices(15, "Lemon", 5, 105.0, new SimpleDateFormat("MM/dd/yyyy").parse("01/01/2020")));
